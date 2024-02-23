8ef3fd344c8d4dac18ff45b91f0872d68e2bf663
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2024-02-24 02:10:31 +0530

    revert changes for autolink files

--- src/main/java/org/jabref/gui/externalfiles/AutoSetFileLinksUtil.java
+++ src/main/java/org/jabref/gui/externalfiles/AutoSetFileLinksUtil.java
@@ -3,7 +3,6 @@ package org.jabref.gui.externalfiles;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -23,7 +22,6 @@ import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.LinkedFile;
-import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.preferences.FilePreferences;
 
@@ -88,9 +86,6 @@ public class AutoSetFileLinksUtil {
                     // store undo information
                     String newVal = FileFieldWriter.getStringRepresentation(linkedFile);
                     String oldVal = entry.getField(StandardField.FILE).orElse(null);
-                    System.out.println("old value==========>"+oldVal);
-                    System.out.println("new value==========>"+newVal);
-                    //field values to be discussed
                     UndoableFieldChange fieldChange = new UndoableFieldChange(entry, StandardField.FILE, oldVal, newVal);
                     ce.addEdit(fieldChange);
                     changed = true;
@@ -108,73 +103,40 @@ public class AutoSetFileLinksUtil {
         return result;
     }
 
-public List<LinkedFile> findAssociatedNotLinkedFiles(BibEntry entry) throws IOException {
-    List<LinkedFile> linkedFiles = new ArrayList<>();
-    System.out.println(entry.getFiles()+" ===========files");
-    List<String> bibFile = entry.getFiles().stream().map(obj -> obj.getLink()).collect(Collectors.toList());
-    System.out.println(bibFile.get(0)+ "=================zero index files");
-    List<String> extensions = filePreferences.getExternalFileTypes().stream()
-            .map(ExternalFileType::getExtension)
-            .collect(Collectors.toList());
+    public List<LinkedFile> findAssociatedNotLinkedFiles(BibEntry entry) throws IOException {
+        List<LinkedFile> linkedFiles = new ArrayList<>();
 
-    // Run the search operation
-    FileFinder fileFinder = FileFinders.constructFromConfiguration(autoLinkPreferences);
-    List<Path> result = new ArrayList<>();
+        List<String> extensions = filePreferences.getExternalFileTypes().stream().map(ExternalFileType::getExtension).collect(Collectors.toList());
 
-    // Search in each directory and its subdirectories
-    for (Path directory : directories) {
-        List<Path> filesInDirectory = Files.walk(directory)
-                .filter(Files::isRegularFile)
-//                .filter(path -> bibFile.contains(path.toString()))
-                .collect(Collectors.toList());
-        System.out.println("files--------->"+filesInDirectory);
-//        result.addAll(fileFinder.findAssociatedFiles(entry, filesInDirectory, extensions));
-        for (Path file : filesInDirectory) {
-            String fileNameDir = file.getFileName().toString();
-            System.out.println(fileNameDir+" dir filename");
-            for (String bibFilePath : bibFile) {
-                Path bibFileNamePath = Paths.get(bibFilePath).getFileName();
-                System.out.println(bibFileNamePath + "bibFileNamePath");
-                System.out.println("bib file: " + bibFilePath);
-                if (bibFileNamePath != null && bibFileNamePath.toString().equals(fileNameDir)) {
-                    result.add(file);
+        // Run the search operation
+        FileFinder fileFinder = FileFinders.constructFromConfiguration(autoLinkPreferences);
+        List<Path> result = fileFinder.findAssociatedFiles(entry, directories, extensions);
 
-                }
+        // Collect the found files that are not yet linked
+        for (Path foundFile : result) {
+            boolean fileAlreadyLinked = entry.getFiles().stream()
+                    .map(file -> file.findIn(directories))
+                    .anyMatch(file -> {
+                        try {
+                            return file.isPresent() && Files.isSameFile(file.get(), foundFile);
+                        } catch (IOException e) {
+                            LOGGER.error("Problem with isSameFile", e);
+                        }
+                        return false;
+                    });
+
+            if (!fileAlreadyLinked) {
+                Optional<ExternalFileType> type = FileUtil.getFileExtension(foundFile)
+                        .map(extension -> ExternalFileTypes.getExternalFileTypeByExt(extension, filePreferences))
+                        .orElse(Optional.of(new UnknownExternalFileType("")));
+
+                String strType = type.isPresent() ? type.get().getName() : "";
+                Path relativeFilePath = FileUtil.relativize(foundFile, directories);
+                LinkedFile linkedFile = new LinkedFile("", relativeFilePath, strType);
+                linkedFiles.add(linkedFile);
             }
         }
-        System.out.println(result);
+
+        return linkedFiles;
     }
-
-    // Collect the found files that are not yet linked
-    for (Path foundFile : result) {
-        System.out.println("foundfile--------->"+foundFile);
-        boolean fileAlreadyLinked = entry.getFiles().stream()
-                .map(file -> file.findIn(directories))
-                .anyMatch(file -> {
-                    try {
-
-                        return file.isPresent() && Files.isSameFile(file.get(), foundFile);
-                    } catch (IOException e) {
-                        LOGGER.error("Problem with isSameFile", e);
-                    }
-                    return false;
-                });
-
-        if (!fileAlreadyLinked) {
-            Optional<ExternalFileType> type = FileUtil.getFileExtension(foundFile)
-                    .map(extension -> ExternalFileTypes.getExternalFileTypeByExt(extension, filePreferences))
-                    .orElse(Optional.of(new UnknownExternalFileType("")));
-
-            String strType = type.isPresent() ? type.get().getName() : "";
-            Path relativeFilePath = FileUtil.relativize(foundFile, directories);
-            LinkedFile linkedFile = new LinkedFile("", relativeFilePath, strType);
-            linkedFiles.add(linkedFile);
-        }
-    }
-
-    return linkedFiles;
-}
-
-
-
 }
